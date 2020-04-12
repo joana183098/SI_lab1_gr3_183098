c70e70c86626b7d2afe8f51520ea35b88762d666
Joana Trajanova <trajanova.joana@gmail.com>
2020-04-12 05:25:40 -0700

    i

--- Item.java
+++ Item.java
@@ -3,13 +3,47 @@ class Item {
 	String name;
 	double price;
 
-	//TODO add variable.
+       char tax = {A, B, V};//TODO add variable.
 
-	//TODO constructor
+	public Item(int ID, string Name, double Price) {//TODO constructor
+		id=ID;
+		name=Name;
+		price=Price;
+	}
+E492: Not an editor command: wg!
 
+	public void setId(int id){
+		this.id=id;
+	}
+        public void setName(string name){
+		this.name=name;
+	}
+	public void setPrice(double price){
+		this.price=price;
+	}
+	public int getID(){
+		return id;
+	}
+	public string getName(){
+		return name;
+	}
+	public double getPrice(){
+		return price;
+	}
 	//TODO setters and getters
 
-	double taxReturn () {
-		//TODO
+	double taxReturn (char tax, double price) {
+	 double taxRet;
+	 switch(tax){
+		 case 'A': 
+		  taxRet = price*0.18;
+	          return taxRet*0.15;
+		 case 'B':
+                  taxRet = price*0.15;
+                  return taxRet*0.15;
+		 default:
+                  taxRet = price*0.0;
+                  return taxRet;//TODO
 	}
-}
+
+} 
